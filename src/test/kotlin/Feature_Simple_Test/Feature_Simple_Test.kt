1fe32204004f019d6b27641ce7fa5bceed35a27e
atdd-bdd <37565969+atdd-bdd@users.noreply.github.com>
2025-02-03 14:06:54 -0500

    Internal Data Class
    Now have internal data working

---
+++ src/test/kotlin/Feature_Simple_Test/Feature_Simple_Test.kt
@@ -0,0 +1,9 @@
+package gherkinexecutor.Feature_Simple_Test
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class Feature_Simple_Test{
+
+        }
+    }
+
